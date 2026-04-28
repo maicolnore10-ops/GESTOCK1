7f7a4f60cdcc21bdcd2703b83603745e5c8469ce
maicolnore10-ops <maicolnore10@gmail.com>
2026-04-28 17:39:42 -0500

    me falto en el main corregir el router, correcion exitosa

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 	router.RegisterUsuariosRoutes(r) 
 		router.RegisterUsuariosRoutes(r)
 		router.RegisterRolesRoutes(r)
+		router.RegisterContraRoutes(r)
 	
 
 	log.Println("Servidor iniciado en http://localhost:8082")
