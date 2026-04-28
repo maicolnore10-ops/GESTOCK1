181b2c69f34a4940106dad4c305eb9cf9f7f3965
maicolnore10-ops <maicolnore10@gmail.com>
2026-04-28 15:52:18 -0500

    se genero el models de roles correctamente

---
+++ models/roles_models.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+// Role representa la estructura de la tabla Autenticacion.roles
+type Role struct {
+	IDRol             int       `json:"id_rol"`
+	NombreRol         string    `json:"nombre_rol"`
+	Activo            bool      `json:"activo"`
+	FechaCreacion     time.Time `json:"fecha_creacion"`
+	FechaModificacion time.Time `json:"fecha_modificacion"`
+}
